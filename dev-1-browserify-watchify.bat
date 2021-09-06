dae0c0de2e445d0f232954a370f48c859154670a
fwg <adf0001@163.com>
2021-09-06 18:54:18 +0800

    update test.htm template

---
+++ dev-1-browserify-watchify.bat
@@ -0,0 +1,12 @@
+
+chcp 936
+
+set watchifyPath="watchify.cmd"
+
+set module=callq
+
+if not exist ./release md release
+
+%watchifyPath% -o ./release/bundle.js -v ^
+	-r ./%module%.js:%module%
+
--- test/test.js
+++ test/test.js
@@ -1,11 +1,9 @@
 
-var assert = require("assert");
-
 showResult= function( txt ) { console.log(txt); }	//global interface
 setHtmlPage= function() {}				//global interface
 
 require("./test-data.js");
 
-describe( 'mocha-test', function () {
+describe( 'mocha-test: callq', function () {
 	for( var i in testData ){ it( i, testData[i] ); }
 });
