b37190ce8a10016ea9c1b3108f3156f238ee67f8
fwg <adf0001@163.com>
2021-10-09 18:08:13 +0800

    update test.htm template

--- dev-1-browserify-watchify.bat
+++ dev-1-browserify-watchify.bat
@@ -1,12 +1,15 @@
 
-chcp 936
-
 set watchifyPath="watchify.cmd"
 
 set module=callq
 
+title watchify - %module%
+
 if not exist ./release md release
 
+for /F %%i in ('npm root -g') do ( set globalModulePath=%%i)
+
 %watchifyPath% -o ./release/bundle.js -v ^
+	-t [ "%globalModulePath%/stringify" --extensions [.html .css .htm ] ] ^
 	-r ./%module%.js:%module%
 
